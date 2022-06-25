c06670a31f5d407b40e6d126c400b8cdc0fc24aa
Cano <contact.sasasaky@gmail.com>
2022-06-25 21:42:32 +0900

    cobra

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
 func main() {
-	println("Hello World!")
+	cmd := &cobra.Command{
+		Use: "go_cli_test",
+		Run: func(c *cobra.Command, args []string) {
+			fmt.Println("Hello World!")
+		},
+	}
+
+	if err := cmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
